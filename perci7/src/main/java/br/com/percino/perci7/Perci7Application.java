d314dd9cf1bdad58f99d2134a7df685ddb702d28
Matheus Percino da Silva <158842558+Matheus-Percino@users.noreply.github.com>
2024-03-03 14:43:49 -0300

    Add files via upload

---
+++ perci7/src/main/java/br/com/percino/perci7/Perci7Application.java
@@ -0,0 +1,13 @@
+package br.com.percino.perci7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Perci7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Perci7Application.class, args);
+	}
+
+}
---
+++ perci7/src/test/java/br/com/percino/perci7/Perci7ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.percino.perci7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Perci7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
